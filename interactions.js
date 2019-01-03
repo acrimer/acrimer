dc04b919686d95cc1b57246bbda1636cf1c02482
Andrew Crimer <andrew.crimer@gmail.com>
2019-01-03 13:24:47 -0500

    all the rest of the work

--- interactions.js
+++ interactions.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(loadPageListeners) {
+  today = new Date();
+  year = today.getFullYear();
+  console.log(year);
+  document.getElementById('year').innerHTML = year;
+
   window.addEventListener('scroll', function(miniNav) {
     header = document.getElementById('header');
     spacer = document.getElementById('spacer');
@@ -13,23 +18,31 @@ document.addEventListener('DOMContentLoaded', function(loadPageListeners) {
     };
   });
   sectionItems = document.getElementsByClassName('section-menu-item');
+  console.log(sectionItems);
   for (var i = 0; i < sectionItems.length; i++) {
     sectionItems[i].addEventListener('click', function(expandProject) {
       clickedItemId = event.target.id;
+      document.getElementById(clickedItemId).classList.toggle('active');
+
       for (var e = 0; e < sectionItems.length; e++) {
         if ( sectionItems[e].classList.contains('active') && ( sectionItems[e].id != clickedItemId) ) {
           sectionItems[e].classList.toggle('active');
         };
       };
+
+       if ( clickedItemId.includes('mob-') ) {
+        clickedItemId = event.target.id.slice(4);
+      } else {
+        clickedItemId = event.target.id;
+      };
+
+      document.getElementById('section-' + clickedItemId).classList.toggle('active');
       projects = document.getElementsByClassName('project');
       for (var a = 0; a < projects.length; a++) {
         if ( projects[a].classList.contains('active') && ( projects[a].id != ('section-' + clickedItemId) ) ) {
           projects[a].classList.toggle('active');
         };
       };
-      clickedItem = document.getElementById(clickedItemId);
-      clickedItem.classList.toggle('active');
-      document.getElementById('section-' + clickedItemId).classList.toggle('active');
     });
   }
 });
